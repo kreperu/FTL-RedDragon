df7118472ec115d614bb11cd9177ed733f520de1
kreperu <thekreperu@gmail.com>
2023-02-19 14:48:16 +0100

    snapshot

--- code.py
+++ code.py
@@ -2,16 +2,21 @@ import time
 import scan
 import comm
 import redirect
+import os
 
 keymap = redirect.getmap()
 
 print("Hello World!")
 b = []
 
+debug = os.getenv("debug")
+
 while True:
+    pt = time.monotonic()
     b = scan.scan()
-    #c = comm.passkey(b, keymap, 0)
-    print(b)
-    #print(c)
-    #comm.release(c, b)
+    c = comm.passkey(b, keymap, 0)
+    comm.release(c, b)
+    ct = time.monotonic()
+    if debug >= 1:
+        print(ct - pt)
     time.sleep(0.0167)
