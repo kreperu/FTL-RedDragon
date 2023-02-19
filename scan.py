445c8f7392fa97b1f3cc7a605b19f5849f017d1d
kreperu <thekreperu@gmail.com>
2023-02-19 13:03:35 +0100

    changes

---
+++ scan.py
@@ -0,0 +1,21 @@
+import digitalio
+import board
+
+rList = [digitalio.DigitalInOut(board.GP4),digitalio.DigitalInOut(board.GP5),digitalio.DigitalInOut(board.GP7),digitalio.DigitalInOut(board.GP8),digitalio.DigitalInOut(board.GP22),digitalio.DigitalInOut(board.GP21),digitalio.DigitalInOut(board.GP20),digitalio.DigitalInOut(board.GP19)]
+
+cList = [digitalio.DigitalInOut(board.GP0),digitalio.DigitalInOut(board.GP1),digitalio.DigitalInOut(board.GP2),digitalio.DigitalInOut(board.GP3),digitalio.DigitalInOut(board.GP6),digitalio.DigitalInOut(board.GP9),digitalio.DigitalInOut(board.GP10),digitalio.DigitalInOut(board.GP11),digitalio.DigitalInOut(board.GP12),digitalio.DigitalInOut(board.GP13),digitalio.DigitalInOut(board.GP14),digitalio.DigitalInOut(board.GP15),digitalio.DigitalInOut(board.GP16),digitalio.DigitalInOut(board.GP17),digitalio.DigitalInOut(board.GP18),digitalio.DigitalInOut(board.GP26)]
+
+def scan():
+    for i in rList:
+        i.direction = digitalio.Direction.INPUT
+        i.pull = digitalio.Pull.UP
+    for i in cList:
+        i.direction = digitalio.Direction.OUTPUT
+        i.value = True
+
+    for i in cList:
+        i.value = False
+        for n in rList:
+            if not n.value:
+
+
